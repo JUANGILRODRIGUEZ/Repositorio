a7b68a5dda710dd79b1f254d6bc4d56889ea2879
JUANGILRODRIGUEZ <jgilrodriguez@itba.edu.ar>
2022-05-31 23:24:10 -0300

    FINAL8

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let Total = 0
+let Total = 0;
 
 function sumarprecio(valorproducto) {
-  Total = Total + valorproducto
-  document.getElementById("Total").innerHTML = "TOTAL: " + "$" + Total + " Pesos";
+  Total = Total + valorproducto;
+  document.getElementById("Total").innerHTML =
+    "TOTAL: " + "$" + Total + " Pesos";
 }
 
 function borrar() {
-  Total=0
-  document.getElementById("Total").innerHTML = "TOTAL: " + "$" + 0 + " Pesos";
+  Total = 0;
+  document.getElementById("Total").innerHTML = 
+    "TOTAL: " + "$" + 0 + " Pesos";
 }
